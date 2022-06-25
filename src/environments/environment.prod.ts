031acda4de6a5588cc3e7063719582decd9c7b76
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-06-25 11:17:09 +0200

    adding env variables in production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  appId: '35e56d4982e25734c82d5a1d0413ea1e',
+  baseUrl: 'https://api.openweathermap.org/data/2.5/weather',
 };
