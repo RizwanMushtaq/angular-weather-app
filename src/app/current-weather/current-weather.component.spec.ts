1f68fb451a9c2c62278d5f672bcb47780dcb1313
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-06-25 21:31:55 +0200

    circle ci and unit testing

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CurrentWeatherComponent } from './current-weather.component';
+import { CurrentWeatherService } from './current-weather.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('CurrentWeatherComponent', () => {
   let component: CurrentWeatherComponent;
@@ -8,10 +9,12 @@ describe('CurrentWeatherComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CurrentWeatherComponent ]
-    })
-    .compileComponents();
+      declarations: [CurrentWeatherComponent],
+      providers: [CurrentWeatherService],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
   });
+  2;
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CurrentWeatherComponent);
